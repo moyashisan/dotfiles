cc7f943fdf7fe2457090aa5416e1405cdd583747
moyashisan <wq59u7h8dy8@gmail.com>
2014-05-24 23:26:31 +0900

    Update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+
+# vimバックアップディレクトリの作成
+mkdir -p ~/.vim/backup
+
+# NeoBundle関係
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
